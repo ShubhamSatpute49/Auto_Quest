5623512c31435744c07d4083f1db3186a0cf0738
ShubhamSatpute <shubhamsatpute4998@gmail.com>
2025-08-20 19:47:39 +0530

    add car schema

---
+++ backend/model/car.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema({
+    brandName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Brand",
+    },
+    showroomId: {
+        type: mongoose.Schema.Types.ObjectId,
+      ref :"Showroom",  
+    },
+    model: {
+        type: String,
+        required: [true, "Model is required"],
+    },
+    price: {
+        type: Number,
+        required: true,
+        min:[0,"Price should be grater than 0"],
+    },
+    discount: {
+        type: Number,
+        default:0,
+        max:[5,"Discount should be less than 5%"]
+    },
+    stock: {
+        type: Number,
+        default: 0,
+    },
+})
+
+const Car = mongoose.model("Car", schema);
+export default Car;
